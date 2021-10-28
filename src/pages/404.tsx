52c121584571b824ed7bb1845e26183fe4b2acc4
Andrew <x.and.i.rew.e@gmail.com>
2021-10-28 22:22:31 +1100

    added 404 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,12 @@
+import React, { useEffect } from "react";
+import { useRouter } from "next/router";
+
+const Unknown: React.FC = () => {
+  const router = useRouter();
+  useEffect(() => {
+    router.push("https://youtu.be/dQw4w9WgXcQ");
+  }, []);
+  return <></>;
+};
+
+export default Unknown;
