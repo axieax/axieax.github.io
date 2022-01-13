50de0d7ca39f2a3ce6cff05a682ff83676b44e16
axieax <62098008+axieax@users.noreply.github.com>
2022-01-13 18:18:13 +0000

    Deploying to gh-pages from @ axieax/axieax.github.io@866686b2cd683efed136fa77e17ecda0f65e5a01 🚀

---
+++ _next/static/5InIOmCT_QR-Ogo0qrq4x/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/e272e97484206e55.css","static/chunks/pages/index-286cd638e73bbac3.js"],"/404":["static/chunks/pages/404-e4fb286610982e58.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/404","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
